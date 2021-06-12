99012a45622698d295a06bd26f8306e603b62c87
CalebObsCode <Sommskycal345@gmail.com>
2021-06-12 15:35:58 -0400

    Login/SignUp Authentication

--- WayneSociety_Project/__init__.py
+++ WayneSociety_Project/__init__.py
@@ -1,7 +1,8 @@
-# __INIT__ to configue ap
+
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager 
 
 # Import Db SQLite + SQLAlchemy
 db = SQLAlchemy()
@@ -9,14 +10,27 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    
+    # This is to configue and setup database
+    app.config['SECRET_KEY'] = 'HHIIDUNUXUU&&DHKJI' #Temporary
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
+    db.init_app(app)
    
+    Set_Login = LoginManager()
+    Set_Login.login_view = 'Routing.Login'
+    Set_Login.init_app(app)
+
+    from .models import User
+
+    @Set_Login.user_loader
+    def Loader_User(Get_User_id):
+        return User.query.get(int(Get_User_id))
+
+
     from .Routing import Routing as Routing_blueprint
     app.register_blueprint(Routing_blueprint)
 
-
-
+    
     from .app import app as app_blueprint
     app.register_blueprint(app_blueprint)
 
