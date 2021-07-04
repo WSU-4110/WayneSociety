35346e0f0915655190f8d9f05432ff2c5de636f9
CalebObsCode <Sommskycal345@gmail.com>
2021-07-04 00:48:32 -0400

    Added Strong Password Functionality

--- WayneSociety_Project/app.py
+++ WayneSociety_Project/app.py
@@ -4,13 +4,3 @@ from . import db
 
 app = Blueprint('app', __name__)
 
-#ToDO: Moved to Sprint 2  ->
-# Implement Efficient Object Oriented Practices for routing
-
-# @app.route('/')
-# def Welcome():
-#     return render_template('Welcome.html')
-
-# @app.route('/profile')
-# def profile():
-#     return render_template('Profile.html' )
