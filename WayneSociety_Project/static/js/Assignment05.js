97984b7d356b3255c88589ea195d3f97cb093412
Anush Rajam <Rajaman3456@gmail.com>
2021-07-07 19:31:24 -0400

    Constructor Design Pattern

---
+++ WayneSociety_Project/static/js/Assignment05.js
@@ -0,0 +1,40 @@
+(
+    function ()
+ {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+        countDown = new Date(birthday).getTime(),
+        x = setInterval(function() {    
+  
+          let now = new Date().getTime(),
+              distance = countDown - now;
+  
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          
+          if (distance < 0) {
+            let headline = document.getElementById("headline"),
+                countdown = document.getElementById("countdown"),
+                content = document.getElementById("content");
+  
+            headline.innerText = "It's Concert Time!";
+            countdown.style.display = "none";
+            content.style.display = "block";
+  
+            clearInterval(x);
+          }
+          
+        }, 0)
+    }()
+    );
+
+    function returnBirthdayString(String){
+        let birthday = "Jul 17, 2021 06:00:00";
+        return birthday;
+    };
