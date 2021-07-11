1d669b764ded8487cb86ebc4a6914128f4adce2d
CalebObsCode <Sommskycal345@gmail.com>
2021-07-11 15:07:57 -0400

    All Changes

--- WayneSociety_Project/HerokuRun.py
+++ WayneSociety_Project/HerokuRun.py
@@ -204,9 +204,13 @@
 #     app.register_blueprint(app_blueprint)
 #     return app
 
-from WayneSociety_Project import create_app
+from __init__ import create_app
 
-app =create_app()
+# from WayneSociety_Project import create_app
+
+
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
